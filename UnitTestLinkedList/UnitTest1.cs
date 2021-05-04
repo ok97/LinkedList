7eb102293e5e2362600d1877bfd3e43794ea639e
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-05-04 18:42:13 +0530

    Added: Ability Search Linked List Find Node

---
+++ UnitTestLinkedList/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedLists;
+namespace UnitTestLinkedList
+{
+    
+    [TestClass]/*TC1 Adding Element in Linked list*/
+    public class UnitTest1
+    {
+        LinkedList list = new LinkedList(); //Create object of LinkedList class
+
+        [TestMethod]
+        public void AddElementLinkedList()
+        {
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+
+[TestMethod] /*TC7*/
+        
+        [DataRow(01, 0)]
+        public void SearchingElementInLinkedList(int data, int position)
+        {
+            
+            //add
+            int actual = list.Search(data);
+            int expected = position;
+            //assert
+            Assert.AreEqual(actual, expected);
+
+        }
+    }
+}
