ccec6a4f9c819c1bcc92e760a4417b367809b8b0
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-05-04 18:53:38 +0530

    Added: Ability to Delete Any Element Linked List

---
+++ UnitTestLinkedList/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedLists;
+namespace UnitTestLinkedList
+{
+    
+    [TestClass]/*TC1 Adding Element in Linked list*/
+    public class UnitTest1
+    {
+        LinkedList list = new LinkedList(); //Create object of LinkedList class
+
+        [TestMethod]
+        public void AddElementLinkedList()
+        {
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+
+      [TestMethod] /*TC7*/
+        
+        [DataRow(01, 0)]
+        public void SearchingElementInLinkedList(int data, int position)
+        {
+            
+            //add
+            int actual = list.Search(data);
+            int expected = position;
+            //assert
+            Assert.AreEqual(actual, expected);
+
+        }
+
+
+[TestMethod] /*TC8*/
+        [DataRow(16, 70)]
+        public void InsertElementInLinkedList(int a, int b)
+        {
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            //values should be in same order, as program class, as while checking for inserting element
+            list.InsertAtAfterValue(a, b);
+            int expected = list.Search(a);
+            int positionForSearchData = list.Search(b);
+            int actual = positionForSearchData + 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+ [TestMethod] /*TC9*/
+        [DataRow(70)]
+        public void DeletingSpecificElementFromList(int data)
+        {
+            list.DeleteElement(data);
+            int expected = 0;
+            int actual = list.Search(70);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
