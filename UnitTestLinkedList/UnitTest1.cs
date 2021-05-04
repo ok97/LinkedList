2a1324fdab01a4859989328aa28bc48f5b48cb60
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-05-04 18:32:02 +0530

    Added: Ability Insert At Position

---
+++ UnitTestLinkedList/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedLists;
+namespace UnitTestLinkedList
+{
+    
+    [TestClass]/*TC1 Adding Element in Linked list*/
+    public class UnitTest1
+    {
+        LinkedList list = new LinkedList(); //Create object of LinkedList class
+
+        [TestMethod]
+        public void AddElementLinkedList()
+        {
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
